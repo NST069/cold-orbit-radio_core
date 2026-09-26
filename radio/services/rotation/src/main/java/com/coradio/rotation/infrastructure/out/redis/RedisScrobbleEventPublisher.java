a4a25d98f917d0e2b40509e5f83bdb4f233ff8a6
NiKryukov <nislav.ns@gmail.com>
2026-09-26 23:10:31 +0300

    Scrobbling feature flag

--- radio/services/rotation/src/main/java/com/coradio/rotation/infrastructure/out/redis/RedisScrobbleEventPublisher.java
+++ radio/services/rotation/src/main/java/com/coradio/rotation/infrastructure/out/redis/RedisScrobbleEventPublisher.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import java.util.Map;
@@ -17,11 +18,18 @@ public class RedisScrobbleEventPublisher implements ScrobbleEventPublisher {
 
     private static final String STREAM = "scrobble-events";
 
+    @Value("${scrobble.enabled}")
+    private boolean enabled;
+
     private final RedisTemplate<String, String> redisTemplate;
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public void publish(ScrobbleEvent event) {
+        if(!enabled){
+            log.info("Skipped scrobble event {}. Scrobbling is disabled", event.eventId());
+            return;
+        }
         try {
             String payload = objectMapper.writeValueAsString(event);
 
